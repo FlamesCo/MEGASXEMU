083fa4cb2770cc64fb563d7bb3fdd25a7b11df08
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-07-23 18:24:27 -0700

    Requet to upload 7.23.22 #0 Stamp Time 6:24 FST~ -

---
+++ MegaSX2EMU.java
@@ -0,0 +1,7 @@
+class MegaSX2EMU {
+    public static void main(String[] args) {
+        System.out.println("MegaSX2EMU // Now loading v1.0x: ");
+        String s = new String();
+        System.out.println(s);
+    }
+}
